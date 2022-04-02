f738166b547cdb2a3bff94ea1b895ac261d2f81e
Jakob Kuehnen <jakobqnen@gmail.com>
2022-04-02 13:28:54 -0500

    Restrict movement to cardinal directions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private GameObject Player;
     public float moveSpeed;
-    // Start is called before the first frame updat
+    public int moveAxis = -1;// -1: stationary, 0: Vertical, 1: Horizontal    
     void Start()
     {
         Player = this.gameObject;
@@ -15,8 +15,60 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float yMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float xMovement = horizontalInput * moveSpeed * Time.deltaTime;
+        float yMovement = verticalInput * moveSpeed * Time.deltaTime;
+        switch (moveAxis)
+        {
+            case -1: // if no movement on previous frame
+                if (verticalInput != 0) // If there is input on vertical axis
+                {
+                    moveAxis = 0;
+                    xMovement = 0; // cancel horizontal movement
+                }
+                else if (horizontalInput != 0) // If no input on vertical but input on horizontal
+                {
+                    moveAxis = 1;
+                    yMovement = 0; // cancel vert movement
+                }
+                // otherwise moveAxis stays at -1
+                break;
+            case 0: // if vertical movement on previous frame
+                if (verticalInput != 0) // if there is still movement on vert
+                {
+                    xMovement = 0;
+                }
+                else if (horizontalInput != 0) // no vert input but hor input
+                {
+                    moveAxis = 1;
+                    yMovement = 0;
+                }
+                else // no input
+                {
+                    xMovement = 0;
+                    yMovement = 0;
+                    moveAxis = -1;
+                }
+                break;
+            case 1:// if horizontal movemont on previous frame
+                if (horizontalInput != 0) // if there is still movement on hor
+                {
+                    yMovement = 0;
+                }
+                else if (verticalInput != 0) // no hor input but vert input
+                {
+                    moveAxis = 0;
+                    xMovement = 0;
+                }
+                else // no input
+                {
+                    xMovement = 0;
+                    yMovement = 0;
+                    moveAxis = -1;
+                }
+                break;
+        }
         Player.transform.localPosition = new Vector2(Player.transform.localPosition.x + xMovement, Player.transform.localPosition.y + yMovement);
     }
 }
