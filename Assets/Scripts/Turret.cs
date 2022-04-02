4cd506068da726edced290aaaa2107be0fde4570
Jakob Kuehnen <jakobqnen@gmail.com>
2022-04-02 15:15:25 -0500

    Add simple turret building

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Turret : ScriptableObject
+{
+    public int cost;
+    public GameObject prefab;
+}
