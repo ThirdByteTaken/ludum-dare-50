b648437480c76bc4eac7cd1e0eead0f01eb607d7
Jakob Kuehnen <jakobqnen@gmail.com>
2022-04-02 12:46:22 -0500

    Add playercontroller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Gameobject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
