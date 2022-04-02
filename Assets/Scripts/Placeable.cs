c750b65a9b97bf7c1535c9409512b690621163c1
Jakob Kuehnen <jakobqnen@gmail.com>
2022-04-02 14:14:54 -0500

    Add object placing

---
+++ Assets/Scripts/Placeable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : MonoBehaviour
+{
+    private GameObject go_PlaceableObject;
+    private Camera cam_Main;
+    public bool bool_Placing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        go_PlaceableObject = this.gameObject;
+        cam_Main = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bool_Placing)
+        {
+            Vector2 mousePosition = DevTools.RoundVector3(cam_Main.ScreenToWorldPoint(Input.mousePosition), 32);
+            go_PlaceableObject.transform.localPosition = mousePosition;
+            if (Input.GetMouseButtonDown(0))
+            {
+                bool_Placing = false;
+            }
+        }
+    }
+}
