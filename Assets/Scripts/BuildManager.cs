c94ab72c47be66146a203b1fd5e14d4f677fb133
Jakob Kuehnen <jakobqnen@gmail.com>
2022-04-02 22:10:25 -0500

    Fix objects while building

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
+    public static BuildManager Instance;
     ResourceManager resourceManager;
-    // Start is called before the first frame update
+    List<GameObject> placedTurrets = new List<GameObject>();
+    GameObject go_currentPlacingTurret;
+    void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
         resourceManager = ResourceManager.Instance;
@@ -14,8 +20,18 @@ public class BuildManager : MonoBehaviour
     public void BuyObject(Turret turret)
     {
         if (resourceManager.Money < turret.cost) return;
-        GameObject.Instantiate(turret.prefab, position: Vector2.zero, rotation: new Quaternion(0, 0, 0, 0));
+        go_currentPlacingTurret = GameObject.Instantiate(turret.prefab, position: Vector2.zero, rotation: new Quaternion(0, 0, 0, 0));
+        placedTurrets.Add(go_currentPlacingTurret);
+        placedTurrets.ForEach(x => x.transform.Find("Range").GetComponent<SpriteRenderer>().enabled = true);
+        go_currentPlacingTurret.GetComponent<BoxCollider2D>().enabled = false;
+        go_currentPlacingTurret.GetComponent<Gun>().enabled = false;
         resourceManager.ChangeCurrency(-turret.cost);
+    }
+    public void StopPlacing()
+    {
+        placedTurrets.ForEach(x => x.transform.Find("Range").GetComponent<SpriteRenderer>().enabled = false);
+        go_currentPlacingTurret.GetComponent<BoxCollider2D>().enabled = true;
+        go_currentPlacingTurret.GetComponent<Gun>().enabled = true;
     }
     // Update is called once per frame
     void Update()
