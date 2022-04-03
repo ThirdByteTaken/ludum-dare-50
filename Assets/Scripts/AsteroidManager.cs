2053fbb6546f65446024611ee00effcf95a48849
Andrew Hartnell <ThirdByteTaken@gmail.com>
2022-04-02 20:07:52 -0700

    Add broken asteroids

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -13,20 +13,17 @@ public class AsteroidManager : MonoBehaviour
     public float landingPositionsRadius;
     public float moveSpeed;
     public GameObject go_AsteroidPrefab;
+
+    public Sprite[] spr_Asteroids;
     void Start()
     {
         SpawnAsteroid();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void SpawnAsteroid()
     {
         GameObject newAsteroid = Instantiate(go_AsteroidPrefab);
+        newAsteroid.GetComponent<SpriteRenderer>().sprite = spr_Asteroids[Random.Range(0, spr_Asteroids.Length)];
         Vector2 newFallVector = new Vector2(Random.Range(-moveSpeed, moveSpeed), Random.Range(-moveSpeed, moveSpeed));
         Vector2 newLandingPosition = new Vector2(Random.Range(-landingPositionsRadius / 2, landingPositionsRadius / 2), Random.Range(-landingPositionsRadius / 2, landingPositionsRadius / 2));
         float newHealth = Random.Range(minHealth, maxHealth);
