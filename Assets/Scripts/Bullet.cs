19b11884c046da28ac4b293b926cef39b16ddb15
Andrew Hartnell <ThirdByteTaken@gmail.com>
2022-04-02 17:52:02 -0700

    Scale out asteroid aim

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,20 +8,46 @@ public class Bullet : MonoBehaviour
 
     public GameObject go_Target;
 
+    Asteroid asteroid;
+
     Vector3 targetPos;
 
     const float Bullet_Speed = 64f;
+    const float Precision = 0.1f; // The lower, the more precise
+
+    float timeToHit; // Amount of time between shooting and bullet hitting target
 
     void Start()
     {
-        if (!Homing) targetPos = go_Target.transform.position;
+        asteroid = go_Target.GetComponent<Asteroid>();
+        if (!Homing)
+        {
+            Vector2 predictedPosition = go_Target.transform.position;
+            float distance; // distance between turret and target
+
+            for (float i = 0; i < 60; i += Precision)
+            {
+                predictedPosition += asteroid.FallVector * Precision;
+                distance = Vector2.Distance(transform.position, predictedPosition);
+                if (Bullet_Speed * i >= distance)
+                {
+                    break;
+                }
+            }
+
+            targetPos = predictedPosition;
+        }
     }
 
     void Update()
     {
-        if (!Homing)
+        if (Homing)
         {
             transform.position = Vector3.MoveTowards(transform.position, go_Target.transform.position, Bullet_Speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, Bullet_Speed * Time.deltaTime);
         }
     }
 }
